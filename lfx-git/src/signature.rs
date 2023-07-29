82dc83729c6f3fa6d270f897892207f26c19a64a
Nathan Fargo <32229490+ntfargo@users.noreply.github.com>
2023-07-29 09:42:45 +0200

    Update signature.rs

--- lfx-git/src/signature.rs
+++ lfx-git/src/signature.rs
@@ -1,5 +1,7 @@
 // Copyright © 2023 Linear Fox. This file is under GPLv3 License.
 
+// This module provides utilities for signing and verifying messages using the Ed25519 digital signature algorithm. 
+
 use ring::signature::{Ed25519KeyPair, Signature};
 use std::error::Error;
 
@@ -22,5 +24,19 @@ impl SignatureUtil {
         let pkcs8_bytes = Ed25519KeyPair::generate_pkcs8(&rng)?;
         let key_pair = Ed25519KeyPair::from_pkcs8(pkcs8_bytes.as_ref())?;
         Ok(key_pair)
+    }
+ 
+    pub fn calculate_s_i(message: &[u8], signature: &Signature, public_key: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
+        let public_key = ring::signature::UnparsedPublicKey::new(&ring::signature::ED25519, public_key);
+        let mut s_i = vec![0; 64];
+        for i in 0..64 {
+            let mut message = message.to_vec();
+            message.push(i as u8);
+            let signature = public_key.verify(message.as_ref(), signature.as_ref());
+            if signature.is_ok() {
+                s_i[i] = 1;
+            }
+        }
+        Ok(s_i)
     }
 }
